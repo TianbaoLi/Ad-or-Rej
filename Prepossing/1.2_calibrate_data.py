749f8b5347d3147dd9d5d9aaf42ab7d7281beb8c
TuringMac <turingmac@hotmail.com>
2017-12-15 00:18:48 -0500

    Calibrated

--- Prepossing/1.2_calibrate_data.py
+++ Prepossing/1.2_calibrate_data.py
@@ -19,18 +19,15 @@ with open('timesData.csv', 'r') as file:
         university_list[name] = rank
         index += 1
 
-ff = open('temp.txt', 'w')
 with open('json_data.txt', 'r') as file:
     for line in file:
         post_dict = json.loads(line)
         post = decode_post(post_dict[u'__Post__'])
 
-        #post.school = calibrate_school(university_list, post.school)
-        #post.degree = calibrate_degree(post.degree)
-        #post.result = calibrate_result(post.result)
-        #post.year = calibrate_year(post.year)
-        #post.toefl_total, post.toefl, post.toefl_reading, post.toefl_listening, post.toefl_speaking, post.toefl_writing = calibrate_toefl(post.toefl)
-        #post.gre_total, post.gre, post.gre_verbal, post.gre_quantity, post.gre_writing = calibrate_gre(post.gre)
+        post.school = calibrate_school(university_list, post.school)
+        post.degree = calibrate_degree(post.degree)
+        post.result = calibrate_result(post.result)
+        post.year = calibrate_year(post.year)
+        post.toefl_total, post.toefl, post.toefl_reading, post.toefl_listening, post.toefl_speaking, post.toefl_writing = calibrate_toefl(post.toefl)
+        post.gre_total, post.gre, post.gre_verbal, post.gre_quantity, post.gre_writing = calibrate_gre(post.gre)
         post.gpa, post.gpa_ranking = calibrate_gpa(post.gpa)
-        ff.write(str(post.gpa) + '\t' + str(post.gpa_ranking) + '\n')
-ff.close()
